25b32cca7614816aa93c101ad6be86375fba1015
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-12-19 10:48:23 -0500

    all links added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,15 @@ const Home = () => {
       <h2>Welcome! Im Sarah</h2>
       <h1>Software Developer</h1>
       <hr className="page-break"></hr>
-      <h3>My Resume</h3>
+      <h3>
+        {' '}
+        <a
+          target="blank"
+          href="https://docs.google.com/document/d/1iRPuzTSrfM7ldXUlzIhw5RCqimNzR56z8AvagCJXk1g/export?format=pdf"
+        >
+          My Resume
+        </a>
+      </h3>
     </div>
   )
 }
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -6,36 +6,51 @@ const ProjectsPage = () => {
     <div>
       <h1 className="home">Portfolio Projects</h1>
       <div className="stock-img">
-        <img
-          style={{
-            width: '500px',
-            height: '400px',
-            margin: '60px',
-            border: '10px hotpink solid',
-            borderRadius: '10px'
-          }}
-          src="https://t3.ftcdn.net/jpg/03/20/68/66/360_F_320686681_Ur6vdYQgDC9WiijiVfxlRyQffxOgfeFz.jpg"
-        />
-        <img
-          style={{
-            width: '500px',
-            height: '400px',
-            margin: '60px',
-            border: '10px hotpink solid',
-            borderRadius: '10px'
-          }}
-          src="https://www.zamperla.com/wp-content/uploads/2014/09/Fimg-Giant_discovery_31024-x-768.jpg"
-        />
-        <img
-          style={{
-            width: '500px',
-            height: '400px',
-            margin: '60px',
-            border: '10px hotpink solid',
-            borderRadius: '10px'
-          }}
-          src="https://sm.mashable.com/t/mashable_sea/photo/default/lofi-anime-girl-hip-hop-chilled-cow_4m7v.1200.jpg"
-        />
+        <a target="blank" href="https://you-buyapp.herokuapp.com/">
+          {' '}
+          <img
+            style={{
+              width: '500px',
+              height: '400px',
+              margin: '40px',
+              border: '10px hotpink solid',
+              borderRadius: '10px',
+              boxShadow: 'black 1px 1px 9px 6px'
+            }}
+            src="https://t3.ftcdn.net/jpg/03/20/68/66/360_F_320686681_Ur6vdYQgDC9WiijiVfxlRyQffxOgfeFz.jpg"
+          />
+          <h2>You-Buy</h2>
+        </a>
+
+        <a target="blank" href="https://thrill-rides.herokuapp.com/">
+          <img
+            style={{
+              width: '500px',
+              height: '400px',
+              margin: '40px',
+              border: '10px hotpink solid',
+              borderRadius: '10px',
+              boxShadow: 'black 1px 1px 9px 6px'
+            }}
+            src="https://www.zamperla.com/wp-content/uploads/2014/09/Fimg-Giant_discovery_31024-x-768.jpg"
+          />
+          <h2>Thrill Rides</h2>
+        </a>
+
+        <a target="blank" href="https://anime-revival-frontend.herokuapp.com/">
+          <img
+            style={{
+              width: '500px',
+              height: '400px',
+              margin: '40px',
+              border: '10px hotpink solid',
+              borderRadius: '10px',
+              boxShadow: 'black 1px 1px 9px 6px'
+            }}
+            src="https://sm.mashable.com/t/mashable_sea/photo/default/lofi-anime-girl-hip-hop-chilled-cow_4m7v.1200.jpg"
+          />{' '}
+          <h2>Anime Revival Blog</h2>
+        </a>
       </div>
     </div>
   )
